62efff030df5653f0bf4cd18c1dd2148e82a43ec
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-29 14:07:26 -0500

    convert to stateless comp and add propTypes

--- src/components/SlideDrawer.jsx
+++ src/components/SlideDrawer.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import Drawer from 'material-ui/Drawer'
 import { IndexLink, Link } from 'react-router'
 import { ListItem } from 'material-ui/List'
@@ -8,31 +8,32 @@ import Project from 'material-ui/svg-icons/action/view-module'
 import About from 'material-ui/svg-icons/social/person'
 import Info from 'material-ui/svg-icons/action/info'
 
-class SlideDrawer extends Component {
-  render() {
-    return (
-      <Drawer
-          width={300}
-          docked={false}
-          open={this.props.open}
-          onRequestChange={(open) => this.props.handleToggle(open) }
-          >
-          <div className='subHeader'>Christopher Wiles</div>
-          <IndexLink to="/" className='link' activeClassName="active" onTouchTap={this.props.handleToggle}>
-            <ListItem primaryText="Home" leftIcon={<Home/>} />
-          </IndexLink>
-          <Link to="/projects" className='link' activeClassName="active"  onTouchTap={this.props.handleToggle}>
-            <ListItem primaryText="Projects" leftIcon={<Project/>} />
-          </Link>
-          <Link to="/about" className='link' activeClassName="active" onTouchTap={this.props.handleToggle}>
-            <ListItem primaryText="About" leftIcon={<About/>} />
-          </Link>
-          <Link to="/info" className='link' activeClassName="active" onTouchTap={this.props.handleToggle}>
-            <ListItem primaryText="Info" leftIcon={<Info/>} />
-          </Link>
-        </Drawer>
-    )
-  }
+const SlideDrawer = ({handleToggle, open}) => (
+  <Drawer
+      width={300}
+      docked={false}
+      open={open}
+      onRequestChange={e => handleToggle(e) }
+      >
+      <div className='subHeader'>Christopher Wiles</div>
+      <IndexLink to="/" className='link' activeClassName="active" onTouchTap={handleToggle}>
+        <ListItem primaryText="Home" leftIcon={<Home/>} />
+      </IndexLink>
+      <Link to="/projects" className='link' activeClassName="active" onTouchTap={handleToggle}>
+        <ListItem primaryText="Projects" leftIcon={<Project/>} />
+      </Link>
+      <Link to="/about" className='link' activeClassName="active" onTouchTap={handleToggle}>
+        <ListItem primaryText="About" leftIcon={<About/>} />
+      </Link>
+      <Link to="/info" className='link' activeClassName="active" onTouchTap={handleToggle}>
+        <ListItem primaryText="Info" leftIcon={<Info/>} />
+      </Link>
+    </Drawer>
+)
+
+SlideDrawer.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleToggle: PropTypes.func.isRequired
 }
 
 export default SlideDrawer
