bac71271c8d49e9c8f419551e005772e092cbadb
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-30 18:59:54 -0500

    convert to stateless comp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,15 @@
-import React, {Component} from 'react'
-import Starry from '../canvas/Starry'
+import React from 'react'
+import Avatar from 'material-ui/Avatar'
 
-class Home extends Component {
-
-  componentDidMount() {
-    const cx = document.getElementById('canvas')
-    const ctx = cx.getContext('2d')
-    let St = new Starry(cx, ctx)
-    St.step()
-    window.onresize = () => {
-      St.cx.width = St.cx.clientWidth
-      St.cx.height = St.cx.clientHeight
-      if (St.dots.length === 0) {
-        St.construct()
-      }
-      St.render()
-    }
-    cx.onmousemove = (e) => {
-      St.mousePos[0] = e.clientX - cx.offsetLeft
-      St.mousePos[1] = e.clientY - 64
-    }
-    window.onresize()
-  }
-
-  render() {
-    return (
-      <div className='canvasbg'>
-        <canvas id='canvas'/>
-        <div className='head'>
-          <span>
-            <img src='https://avatars3.githubusercontent.com/u/9455409?v=3&s=466' role="presentation"/>
-          </span>
-          <h2>Christopher Wiles</h2>
-          <h3>Software Engineer</h3>
-        </div>
-      </div>
-    )
-  }
-}
+const Home = () => (
+  <div className='head'>
+    <Avatar
+      className='Home__Avatar'
+      src="https://avatars3.githubusercontent.com/u/9455409?v=3&s=466"
+      size={150}/>
+    <h2>Christopher Wiles</h2>
+    <h3>Software Engineer</h3>
+  </div>
+)
 
 export default Home
