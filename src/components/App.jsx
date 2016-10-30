b53ad00a12e14dcdb8aeb82daef729300c099b6a
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-30 18:59:25 -0500

    move canvas to App and make fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 
+import Starry from '../canvas/Starry'
+
 import NavBar from './NavBar'
 import SlideDrawer from './SlideDrawer'
 
@@ -8,17 +10,42 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      open: false
+      open: false,
+      title: 'Home'
     }
+  }
+
+  componentDidMount() {
+    const cx = document.getElementById('canvas')
+    const ctx = cx.getContext('2d')
+    let St = new Starry(cx, ctx)
+    St.step()
+    window.onresize = () => {
+      St.cx.width = St.cx.clientWidth
+      St.cx.height = St.cx.clientHeight
+      if (St.dots.length === 0) {
+        St.construct()
+      }
+      St.render()
+    }
+    cx.onmousemove = (e) => {
+      St.mousePos[0] = e.clientX - cx.offsetLeft
+      St.mousePos[1] = e.clientY - 64
+    }
+    window.onresize()
   }
 
   handleToggle = () => this.setState({open: !this.state.open})
 
+  setTitle = title => this.setState({title})
+
   render() {
+    const {title, open} = this.state
     return (
       <div>
-        <NavBar handleToggle={this.handleToggle}/>
-        <SlideDrawer handleToggle={this.handleToggle} open={this.state.open}/>
+        <canvas id='canvas'/>
+        <NavBar handleToggle={this.handleToggle} title={title}/>
+        <SlideDrawer handleToggle={this.handleToggle} open={open} setTitle={this.setTitle}/>
 
         { /* child component will be rendered here */ }
         {this.props.children}
