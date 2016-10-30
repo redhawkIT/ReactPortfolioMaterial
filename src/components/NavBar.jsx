db16c55f5fc0ef08a58a4b7d802da577e751dff2
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-30 17:01:51 -0500

    css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton'
+import Menu from 'material-ui/svg-icons/navigation/menu'
 
 class NavBar extends Component {
 
   render() {
     return (
       <AppBar
-          iconElementRight={<IconButton iconClassName="muidocs-icon-custom-github"  href='https://github.com/ChrisWiles'/>}
+          iconElementLeft={<IconButton><Menu className="AppBar__Menu"/></IconButton>}
+          iconElementRight={<IconButton  iconClassName="muidocs-icon-custom-github"  href='https://github.com/ChrisWiles'/>}
           style={{background: 'none', position:'fixed'}}
           zDepth={0}
           onLeftIconButtonTouchTap={this.props.handleToggle}
