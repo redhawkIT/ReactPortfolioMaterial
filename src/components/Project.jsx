db01e434189ad34af9447e10d809734bb850d0ca
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-30 19:51:05 -0500

    link btn to href

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,14 +4,14 @@ import FlatButton from 'material-ui/FlatButton'
 
 const Project = ({text, img, title, tech, github_url, project_url, date}) => (
   <Card style={{backgroundColor: 'none', boxShadow: 'none'}}>
+    <CardTitle title={title} titleColor='white' subtitleColor='white' subtitle={tech.join(', ')} />
     <CardMedia>
-      <img className="Projects__img" src={img} role="presentation"/>
+      <img src={img} role="presentation"/>
     </CardMedia>
-    <CardTitle title={title} titleColor='white' subtitleColor='white' subtitle={tech.join('')} />
     <CardText className="Project__text">{text}</CardText>
     <CardActions>
-      <FlatButton className="Project__Button" label="Code" />
-      <FlatButton className="Project__Button" label="Live Demo" />
+      <FlatButton className="Project__Button" label="Code"  href={github_url}/>
+      <FlatButton className="Project__Button" label="Live Demo"  href={project_url}/>
     </CardActions>
   </Card>
 )
