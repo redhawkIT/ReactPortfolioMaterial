744cee5ade69153c1c0808daea15f6d889569f9a
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-11-24 08:55:47 -0600

    format

--- src/projects.js
+++ src/projects.js
@@ -1,188 +1,188 @@
 export default[
-  {
-    img : 'http://i.imgur.com/4o2p9q6.gif',
-    mobile_image : '',
-    title : 'Github Viewer',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/githubViewer',
-    project_url : 'https://chriswiles.github.io/githubViewer',
-    text : 'Uses Apollo to connect the client with GitHub’s GraphQL. The queries are saved into the redux store and the UI is rendered with React and Material UI components',
-    role : 'Full Stack',
-    tech : [
-      'Apollo Client',
-      'GitHub GraphQL API',
-      'GraphQL',
-      'Material UI',
-      'React',
-      'Redux'
-    ]
-  }, {
-    img : 'http://i.imgur.com/ruYOFwT.gif',
-    mobile_image : 'http://i.imgur.com/JuEGyoT.jpg',
-    title : 'Melody Map',
-    date : '2016',
-    github_url : 'https://github.com/WaterlooATX/MelodyMap',
-    project_url : 'http://melody-map.com/',
-    text : 'Discover live music in your area tonight. With Melody Map, you can easily explore artists, venues, and shows happening in your city and beyond. See when your favorite bands are playing, check out that new venue, or give a listen and discover new artists.',
-    role : 'Full Stack',
-    // tech : [
-    //   {
-    //     img: require('./techLogos/react.png')
-    //   }, {
-    //     img: require('./techLogos/mongodb.png')
-    //   }, {
-    //     img: require('./techLogos/redux.png')
-    //   }, {
-    //     img: require('./techLogos/node.png')
-    //   }, {
-    //     img: require('./techLogos/bootstrap.png')
-    //   }
-    // ]
-    tech : [
-      'React',
-      'MongoDB',
-      'Redux',
-      'Bootstrap',
-      'Node',
-      'Express',
-      'lodash',
-      'React Router',
-      'redux-promise',
-      'spotify API',
-      'youtube API',
-      'google maps API',
-      'songkick API',
-      'last FM API',
-      'google Places API'
-    ]
-  }, {
-    img : 'http://i.imgur.com/PTBaev5.gif',
-    mobile_image : '',
-    title : 'Slider-Puzzle',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/Slide-Puzzle',
-    project_url : 'https://slider-puzzle.herokuapp.com/',
-    text : 'A sliding puzzle that consists of a frame of numbered square tiles in random order with one tile missing. The n-puzzle is a classical problem for modeling algorithms. The object of the puzzle is to place the tiles in order by making sliding moves that use the empty space. Bobby Fischer was an expert at solving the 15-Puzzle. He had been timed to be able to solve it within 25 seconds',
-    tech : ['React', 'Material-UI', 'Express', 'MongoDB', 'Node']
-  }, {
-    img : 'http://i.imgur.com/Hn2l7h7.gif',
-    mobile_image : '',
-    title : 'Game of Life',
-    date : '2015',
-    github_url : 'https://github.com/ChrisWiles/Game-of-Life-canvas',
-    project_url : 'http://htmlpreview.github.io/?https://github.com/ChrisWiles/Game-of-Life-canvas/blob/master/index.html',
-    text : 'The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. Any live cell with fewer than two live neighbors dies, as if caused by under-population. Any live cell with two or three live neighbors lives on to the next generation. Any live cell with more than three live neighbors dies, as if by over-population. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.',
-    role : null,
-    tech : ['HTML5-Canvas', 'jQuery']
-  }, {
-    img : 'http://imgur.com/PSXi7bu.jpg',
-    mobile_image : '',
-    title : 'Buzz Finder',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/buzzFinder',
-    project_url : 'https://www.youtube.com/watch?v=6SVX3OjDIrk',
-    text : 'Local Craft Beer - Either search through a list breweries near you, or search breweries by city and state. View brewery details and save your favorites. Share where you have been and recommend breweries to friends. Cheers!',
-    role : null,
-    tech : ['React', 'Bootstrap']
-  }, {
-    img : 'http://i.imgur.com/vxP3k1m.gif',
-    mobile_image : '',
-    title : 'Youtube Autocomplete Search Bar',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/YouTube-Material-UI',
-    project_url : 'https://youtube-autocomplete.herokuapp.com/',
-    text : 'Calls googleAutoSuggest when a user types, maps over googles auto suggest data and passes the array to Material UIs AutoComplete component',
-    tech : ['React', 'Material-UI', 'Youtube API', 'Google Suggestqueries API']
-  }, {
-    img : 'http://i.imgur.com/ByQkwVO.jpg',
-    mobile_image : 'http://i.imgur.com/aAw02Us.jpg',
-    title : 'Gomoku',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/GomokuReact',
-    project_url : 'https://gomoku-atx.herokuapp.com/',
-    text : 'Gomoku is an abstract strategy board game. Also called Gobang or Five in a Row, it is traditionally played with Go pieces (black and white stones) on a go board with 19x19 (15x15) intersections. The winner is the first player to get an unbroken row of five stones horizontally, vertically, or diagonally.',
-    role : null,
-    tech : ['React', 'socket.io', 'Bootstrap', 'Node', 'Express']
-  }, {
-    img : 'http://i.imgur.com/KfDqw3v.gif',
-    mobile_image : '',
-    title : 'Character Frequency',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/CharacterFrequency',
-    project_url : 'https://character-frequency.herokuapp.com/',
-    text : 'Character Frequency is a cloud-enabled, offline-storage, ReactJS powered data visualizer. Analyzes text and display the number of times all users have entered it. Data is stored in a Mongo DB and visualized with a bar and pie chart using Victory.js',
-    tech : [
-      'React',
-      'Victory.js',
-      'Mongoose',
-      'Node',
-      'Express',
-      'Bootstrap'
-    ]
-  }, {
-    img : 'http://i.imgur.com/mOtWsT8.gif',
-    mobile_image : '',
-    title : 'Simon Says',
-    date : '2015',
-    github_url : 'https://github.com/ChrisWiles/Simon-Says',
-    project_url : 'http://htmlpreview.github.io/?https://github.com/ChrisWiles/Simon-Says/blob/master/index.html',
-    text : 'Uses HTMLl5 audio features and jQuery. The device has four colored buttons, each producing a particular tone when it is pressed or activated by the device. A round in the game consists of the device lighting up one or more buttons in a random order, after which the player must reproduce that order by pressing the buttons. As the game progresses, the number of buttons to be pressed increases.',
-    role : null,
-    tech : ['jQuery', 'Bootstrap']
-  }, {
-    img : 'http://i.imgur.com/xxZ2E29.gif',
-    mobile_image : '',
-    title : 'Snake',
-    date : '2014',
-    github_url : 'https://github.com/ChrisWiles/snake-game',
-    project_url : 'http://goo.gl/5hp8X5',
-    text : 'One of my favorite games of all time - Snake! I wanted to create it from my first day with JavaScript and the nostalgia was worth it!',
-    role : null,
-    tech : ['jQuery', 'HTML5-Canvas']
-  }, {
-    img : 'http://i.imgur.com/Z5VGb4U.jpg',
-    mobile_image : '',
-    title : 'Vanilla JS Calculator',
-    date : '2014',
-    github_url : 'https://github.com/ChrisWiles/Vanilla-JS-Calculator',
-    project_url : 'https://htmlpreview.github.io/?https://github.com/ChrisWiles/Vanilla-JS-Calculator/blob/master/index.html',
-    text : 'A simple calculator built using HTML, CSS and JavaScript.',
-    role : null,
-    tech : ['CSS', 'HTML', 'JavaScript']
-  }, {
-    img : '',
-    mobile_image : '',
-    title : 'Granite Parchment Shears',
-    date : '2016',
-    github_url : 'https://github.com/striking-lunatics/RockPaperScissors',
-    project_url : 'http://granite-parchment-shears.herokuapp.com/',
-    text : 'A two player game of Rock-Paper-Scissors. Create a new game and access code, invite a friend and THROW! Enjoy seamless and instantaneous game play.',
-    tech : [
-      'React',
-      'React Router',
-      'socket.io',
-      'Express',
-      'PostgreSQL',
-      'Bootstrap'
-    ]
-  }, {
-    img : 'http://i.imgur.com/e8aGCwQ.jpg',
-    mobile_image : '',
-    title : 'Toy Problems',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/toyProblems',
-    project_url : 'https://github.com/ChrisWiles/toyProblems',
-    text : 'My personal solutions to programming challenges I come across.',
-    role : null,
-    tech : ['JavaScript']
-  }, {
-    img : '',
-    mobile_image : '',
-    title : 'React Portfolio',
-    date : '2016',
-    github_url : 'https://github.com/ChrisWiles/ReactPortfolioMaterial',
-    project_url : 'https://chriswiles.github.io/',
-    text : 'Built this portfolio using React and Material UI',
-    tech : ['React', 'Material-UI, React Router']
-  }
+   {
+      img : 'http://i.imgur.com/4o2p9q6.gif',
+      mobile_image : '',
+      title : 'Github Viewer',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/githubViewer',
+      project_url : 'https://chriswiles.github.io/githubViewer',
+      text : 'Uses Apollo to connect the client with GitHub’s GraphQL. The queries are saved into the redux store and the UI is rendered with React and Material UI components',
+      role : 'Full Stack',
+      tech : [
+         'Apollo Client',
+         'GitHub GraphQL API',
+         'GraphQL',
+         'Material UI',
+         'React',
+         'Redux'
+      ]
+   }, {
+      img : 'http://i.imgur.com/ruYOFwT.gif',
+      mobile_image : 'http://i.imgur.com/JuEGyoT.jpg',
+      title : 'Melody Map',
+      date : '2016',
+      github_url : 'https://github.com/WaterlooATX/MelodyMap',
+      project_url : 'http://melody-map.com/',
+      text : 'Discover live music in your area tonight. With Melody Map, you can easily explore artists, venues, and shows happening in your city and beyond. See when your favorite bands are playing, check out that new venue, or give a listen and discover new artists.',
+      role : 'Full Stack',
+      // tech : [
+      //   {
+      //     img: require('./techLogos/react.png')
+      //   }, {
+      //     img: require('./techLogos/mongodb.png')
+      //   }, {
+      //     img: require('./techLogos/redux.png')
+      //   }, {
+      //     img: require('./techLogos/node.png')
+      //   }, {
+      //     img: require('./techLogos/bootstrap.png')
+      //   }
+      // ]
+      tech : [
+         'React',
+         'MongoDB',
+         'Redux',
+         'Bootstrap',
+         'Node',
+         'Express',
+         'lodash',
+         'React Router',
+         'redux-promise',
+         'spotify API',
+         'youtube API',
+         'google maps API',
+         'songkick API',
+         'last FM API',
+         'google Places API'
+      ]
+   }, {
+      img : 'http://i.imgur.com/PTBaev5.gif',
+      mobile_image : '',
+      title : 'Slider-Puzzle',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/Slide-Puzzle',
+      project_url : 'https://slider-puzzle.herokuapp.com/',
+      text : 'A sliding puzzle that consists of a frame of numbered square tiles in random order with one tile missing. The n-puzzle is a classical problem for modeling algorithms. The object of the puzzle is to place the tiles in order by making sliding moves that use the empty space. Bobby Fischer was an expert at solving the 15-Puzzle. He had been timed to be able to solve it within 25 seconds',
+      tech : ['React', 'Material-UI', 'Express', 'MongoDB', 'Node']
+   }, {
+      img : 'http://i.imgur.com/Hn2l7h7.gif',
+      mobile_image : '',
+      title : 'Game of Life',
+      date : '2015',
+      github_url : 'https://github.com/ChrisWiles/Game-of-Life-canvas',
+      project_url : 'http://htmlpreview.github.io/?https://github.com/ChrisWiles/Game-of-Life-canvas/blob/master/index.html',
+      text : 'The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. Any live cell with fewer than two live neighbors dies, as if caused by under-population. Any live cell with two or three live neighbors lives on to the next generation. Any live cell with more than three live neighbors dies, as if by over-population. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.',
+      role : null,
+      tech : ['HTML5-Canvas', 'jQuery']
+   }, {
+      img : 'http://imgur.com/PSXi7bu.jpg',
+      mobile_image : '',
+      title : 'Buzz Finder',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/buzzFinder',
+      project_url : 'https://www.youtube.com/watch?v=6SVX3OjDIrk',
+      text : 'Local Craft Beer - Either search through a list breweries near you, or search breweries by city and state. View brewery details and save your favorites. Share where you have been and recommend breweries to friends. Cheers!',
+      role : null,
+      tech : ['React', 'Bootstrap']
+   }, {
+      img : 'http://i.imgur.com/vxP3k1m.gif',
+      mobile_image : '',
+      title : 'Youtube Autocomplete Search Bar',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/YouTube-Material-UI',
+      project_url : 'https://youtube-autocomplete.herokuapp.com/',
+      text : 'Calls googleAutoSuggest when a user types, maps over googles auto suggest data and passes the array to Material UIs AutoComplete component',
+      tech : ['React', 'Material-UI', 'Youtube API', 'Google Suggestqueries API']
+   }, {
+      img : 'http://i.imgur.com/ByQkwVO.jpg',
+      mobile_image : 'http://i.imgur.com/aAw02Us.jpg',
+      title : 'Gomoku',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/GomokuReact',
+      project_url : 'https://gomoku-atx.herokuapp.com/',
+      text : 'Gomoku is an abstract strategy board game. Also called Gobang or Five in a Row, it is traditionally played with Go pieces (black and white stones) on a go board with 19x19 (15x15) intersections. The winner is the first player to get an unbroken row of five stones horizontally, vertically, or diagonally.',
+      role : null,
+      tech : ['React', 'socket.io', 'Bootstrap', 'Node', 'Express']
+   }, {
+      img : 'http://i.imgur.com/KfDqw3v.gif',
+      mobile_image : '',
+      title : 'Character Frequency',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/CharacterFrequency',
+      project_url : 'https://character-frequency.herokuapp.com/',
+      text : 'Character Frequency is a cloud-enabled, offline-storage, ReactJS powered data visualizer. Analyzes text and display the number of times all users have entered it. Data is stored in a Mongo DB and visualized with a bar and pie chart using Victory.js',
+      tech : [
+         'React',
+         'Victory.js',
+         'Mongoose',
+         'Node',
+         'Express',
+         'Bootstrap'
+      ]
+   }, {
+      img : 'http://i.imgur.com/mOtWsT8.gif',
+      mobile_image : '',
+      title : 'Simon Says',
+      date : '2015',
+      github_url : 'https://github.com/ChrisWiles/Simon-Says',
+      project_url : 'http://htmlpreview.github.io/?https://github.com/ChrisWiles/Simon-Says/blob/master/index.html',
+      text : 'Uses HTMLl5 audio features and jQuery. The device has four colored buttons, each producing a particular tone when it is pressed or activated by the device. A round in the game consists of the device lighting up one or more buttons in a random order, after which the player must reproduce that order by pressing the buttons. As the game progresses, the number of buttons to be pressed increases.',
+      role : null,
+      tech : ['jQuery', 'Bootstrap']
+   }, {
+      img : 'http://i.imgur.com/xxZ2E29.gif',
+      mobile_image : '',
+      title : 'Snake',
+      date : '2014',
+      github_url : 'https://github.com/ChrisWiles/snake-game',
+      project_url : 'http://goo.gl/5hp8X5',
+      text : 'One of my favorite games of all time - Snake! I wanted to create it from my first day with JavaScript and the nostalgia was worth it!',
+      role : null,
+      tech : ['jQuery', 'HTML5-Canvas']
+   }, {
+      img : 'http://i.imgur.com/Z5VGb4U.jpg',
+      mobile_image : '',
+      title : 'Vanilla JS Calculator',
+      date : '2014',
+      github_url : 'https://github.com/ChrisWiles/Vanilla-JS-Calculator',
+      project_url : 'https://htmlpreview.github.io/?https://github.com/ChrisWiles/Vanilla-JS-Calculator/blob/master/index.html',
+      text : 'A simple calculator built using HTML, CSS and JavaScript.',
+      role : null,
+      tech : ['CSS', 'HTML', 'JavaScript']
+   }, {
+      img : '',
+      mobile_image : '',
+      title : 'Granite Parchment Shears',
+      date : '2016',
+      github_url : 'https://github.com/striking-lunatics/RockPaperScissors',
+      project_url : 'http://granite-parchment-shears.herokuapp.com/',
+      text : 'A two player game of Rock-Paper-Scissors. Create a new game and access code, invite a friend and THROW! Enjoy seamless and instantaneous game play.',
+      tech : [
+         'React',
+         'React Router',
+         'socket.io',
+         'Express',
+         'PostgreSQL',
+         'Bootstrap'
+      ]
+   }, {
+      img : 'http://i.imgur.com/e8aGCwQ.jpg',
+      mobile_image : '',
+      title : 'Toy Problems',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/toyProblems',
+      project_url : 'https://github.com/ChrisWiles/toyProblems',
+      text : 'My personal solutions to programming challenges I come across.',
+      role : null,
+      tech : ['JavaScript']
+   }, {
+      img : '',
+      mobile_image : '',
+      title : 'React Portfolio',
+      date : '2016',
+      github_url : 'https://github.com/ChrisWiles/ReactPortfolioMaterial',
+      project_url : 'https://chriswiles.github.io/',
+      text : 'Built this portfolio using React and Material UI',
+      tech : ['React', 'Material-UI, React Router']
+   }
 ]
